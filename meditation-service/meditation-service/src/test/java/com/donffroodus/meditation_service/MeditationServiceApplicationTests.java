f7b6afaaa10768b20a7ec991478bf3f0dc4b78b6
4ofDiamonds <donffroodus@gmail.com>
2026-04-17 12:02:56 +0800

    创建 meditation-service，api-gateway 配置路由转发

---
+++ meditation-service/meditation-service/src/test/java/com/donffroodus/meditation_service/MeditationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donffroodus.meditation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
