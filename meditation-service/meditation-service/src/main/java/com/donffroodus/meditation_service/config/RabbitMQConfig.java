379cda955f388b3581f6a0e74fbb93b646695ae4
4ofDiamonds <donffroodus@gmail.com>
2026-05-04 16:10:24 +0800

    新增 RabbitMQ 配置和消息转换器，更新冥想日志删除逻辑

--- meditation-service/meditation-service/src/main/java/com/donffroodus/meditation_service/config/RabbitMQConfig.java
+++ meditation-service/meditation-service/src/main/java/com/donffroodus/meditation_service/config/RabbitMQConfig.java
@@ -1,11 +1,14 @@
 package com.donffroodus.meditation_service.config;
 
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 
+@Configuration
 public class RabbitMQConfig {
     @Bean
-    public TopicExchange userExchange() {
-        return new TopicExchange("user.exchange");
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
